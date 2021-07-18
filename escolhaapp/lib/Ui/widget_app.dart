8f5913de1ff53a0c9f80abe190b5a8188cc31015
Lucas Nogueira <lucasgo1099@gmail.com>
2021-07-18 15:32:56 -0300

    finish project

--- escolhaapp/lib/Ui/widget_app.dart
+++ escolhaapp/lib/Ui/widget_app.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
+//Defiinido layoult, stagul vai tratar com integração do usuário que vai receber um estado
 class Home extends StatefulWidget {
   //const Home({ Key? key }) : super(key: key);
 
@@ -26,25 +27,34 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.orange,
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.refresh), onPressed: () {})
+          IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                //metodo refresh
+                _resetaValores();
+              })
         ],
       ),
-      body: Form(
-        key: _formKey,
-        //GLobal key para validar o formulário
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(
-              Icons.directions_car,
-              color: Colors.orange,
-              size: 60.0,
-            ),
-            TextFormGasolina(),
-            TextFormAlcool(),
-            BuildButtonSave(context),
-            buildTextInfo(),
-          ],
+      // Widget(SingleChildScrollView) para rolagem de tela
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(20.0, 0, 20.0, 0),
+        child: Form(
+          key: _formKey,
+          //GLobal key para validar o formulário
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(
+                Icons.directions_car,
+                color: Colors.orange,
+                size: 60.0,
+              ),
+              TextFormGasolina(),
+              TextFormAlcool(),
+              BuildButtonSave(context),
+              buildTextInfo(),
+            ],
+          ),
         ),
       ),
     );
@@ -66,12 +76,13 @@ class _HomeState extends State<Home> {
       decoration: InputDecoration(
           labelText: "Preço da Gasolina",
           labelStyle: TextStyle(color: Colors.black, fontSize: 20.0)),
+      style: TextStyle(fontSize: 30.0),
       //Controlar os dados digitados pelo usuário
       controller: gasolinaController,
       //validação pra fazer
       validator: (value) {
         //SE o valor for vázio
-        if (value.isEmpty || value == null) {
+        if (value.isEmpty ?? value == null) {
           return 'Informe o valor da Gasolina';
         } else {
           return null;
@@ -87,6 +98,7 @@ class _HomeState extends State<Home> {
         decoration: InputDecoration(
             labelText: "Preço da Alcool",
             labelStyle: TextStyle(color: Colors.black, fontSize: 20.0)),
+        style: TextStyle(fontSize: 30.0),
         controller: alcoolController,
         //validação pra fazer
         validator: (value) {
@@ -107,10 +119,10 @@ class _HomeState extends State<Home> {
       height: 50.0,
       child: RaisedButton(
         onPressed: () {
-          //validando o formulário
+          //SE as informações do formulario tenha sido preenchidas
           if (_formKey.currentState.validate()) {
-            //chamando o metodo calcular
-
+            //chamando o metodo calcular metod calcula();
+            calcula();
             //colocando foco no campo quando chega o usuário
             FocusScope.of(context).requestFocus(new FocusNode());
           }
@@ -122,5 +134,34 @@ class _HomeState extends State<Home> {
         color: Colors.orange,
       ),
     );
+  }
+
+  // função para calcular
+  void calcula() {
+    setState(() {
+      //obter os valores que foram inseridos pelo usuários
+      double gasolina = double.parse(gasolinaController.text);
+      double alcool = double.parse(alcoolController.text);
+
+      //variavel resultado
+      double resultado = (alcool / gasolina);
+      //fazendo verificação
+      if (resultado > 0.70) {
+        //toStringAsPrecision vai retornar 3 digitos signigicativos
+        _infoText =
+            "Percentual : (${resultado.toStringAsPrecision(3)})\n\n Vale a pena Abastecer com Gasolina";
+      } else {
+        _infoText =
+            "Percentual : (${resultado.toStringAsPrecision(3)})\n\n Vale a pena Abastecer com alcool";
+      }
+    });
+  }
+
+  _resetaValores() {
+    setState(() {
+      gasolinaController.text = "";
+      alcoolController.text = "";
+      _infoText = "Informe o valor de cada combustível";
+    });
   }
 }
